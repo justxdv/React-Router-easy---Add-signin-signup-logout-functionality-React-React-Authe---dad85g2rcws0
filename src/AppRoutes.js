f56753f493c4c891f138b4bbe66b7410283a480f
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-02-24 12:27:50 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Index from './pages/Index';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
-export const AppRoutes = () => {
-    return (
+const AppRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/home" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+};
 
-    )
-}
+export default AppRoutes;
